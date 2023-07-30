b7792d7fa4c9c97ade7be6f03ec0625e7ac5f2aa
bashar samir <basharsamir@MacBook.local>
2023-07-30 15:01:29 +0300

    title remive

--- src/redux/reducer/moviesReducer.js
+++ src/redux/reducer/moviesReducer.js
@@ -1,7 +1,6 @@
 import { AllMOVIES } from "../types/mpviesType";
 const initalValue = {movies:[],pageCount:0}
 export  const moviesReducer= (state=initalValue,action)=>{
-    // eslint-disable-next-line default-case
     switch(action.type)
     {
         case AllMOVIES:
